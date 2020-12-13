511dde54a5e053ff887e05673188a8fc22b57cd6
Harshit Maurya <hmaurya999@gmail.com>
2020-12-13 15:34:52 +0530

    create simple http server

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	_, err := fmt.Fprintf(w, "Welcome to the HomePage!")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Endpoint hit: Homepage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
 
 func main() {
 	fmt.Println("Hello World!")
+	handleRequests()
 }
